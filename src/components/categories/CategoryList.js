a2bc81a6d217b940b8d4d5bf8e2d78c214e970be
emnylmz <eminyilmazz96@gmail.com>
2021-10-24 02:41:12 +0300

    select category - bootstrap css fix

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react'
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
 import * as categoryActions from "../../redux/actions/categoryActions"
-import { ListGroup, ListGroupItem } from "reactstrap"
+import { ListGroup, ListGroupItem,Badge } from "reactstrap"
 
 class CategoryList extends Component {
     componentDidMount() {
         this.props.actions.getCategories();
     }
+    selectCategory = (category) => {
+        this.props.actions.changeCategory(category)
+    }
     render() {
         return (
             <div>
-                <h3>Categories {this.props.categories.length}</h3>
+                <h3>
+                    <Badge color="warning">Categories</Badge>
+                    </h3>
                 <ListGroup>
                     {this.props.categories.map(category => (
-                        <ListGroupItem active={category.id==this.props.currentCategory.id} key={category.id}>
+                        <ListGroupItem onClick={() => this.selectCategory(category)} active={category.id == this.props.currentCategory.id} key={category.id}>
                             {category.categoryName}
                         </ListGroupItem>
                     ))}
                 </ListGroup>
-                <h5>Seçili Kategori:{this.props.currentCategory.categoryName}</h5>
             </div>
         )
     }
@@ -28,7 +32,8 @@ class CategoryList extends Component {
 function mapDispatchToProps(dispatch) {
     return {
         actions: {
-            getCategories: bindActionCreators(categoryActions.getCategories, dispatch)
+            getCategories: bindActionCreators(categoryActions.getCategories, dispatch),
+            changeCategory: bindActionCreators(categoryActions.changeCategory, dispatch)
         }
     }
 }
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,6 +1,4 @@
 export default {
-    currentCategory: {
-        categoryName: "test"
-    },
+    currentCategory: {},
     categories: []
 }
